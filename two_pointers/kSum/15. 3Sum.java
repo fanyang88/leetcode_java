e45d484975d8f657cb9aecc22dc9ef829b4c8253
Fan Yang <1988.fan.yang@gmail.com>
2022-07-11 10:01:21 -0700

    python

--- two_pointers/kSum/15. 3Sum.java
+++ two_pointers/kSum/15. 3Sum.java
@@ -45,3 +45,48 @@ class Solution {
       return res;
   }
 }
+
+
+PYTHON:
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        length = len(nums)
+        for i in range(0, length-2):
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+                
+            j, k = i+1, length-1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] == 0:
+                    #record it
+                    res.append([nums[i], nums[j], nums[k]])
+                    j+=1
+                    while nums[j] == nums[j-1]  and j<k: 
+                        j+=1
+                    k-=1
+                     
+                elif nums[i] + nums[j] + nums[k] < 0:
+                    j+=1
+                else:
+                    k-=1
+                
+        return res
+            
+        
+        
+# Thoughts: we can sort this array first
+#     [-1,0,1,2,-1,-4]
+#     [-4,-1,-1,0, 1, 2]
+    
+#     fix -4 first, l->-1, r->2 sum< 0 l++ [i]=[i++] l++
+#                   l->0, r->2 sum< 0 l++
+#                   l->1, r->2 sum< 0 l++ exit
+            
+#     fix -1        l->-1, r->2 sum= 0 res append [-1, -1, 2] l++, r-- 
+#                   l->0, r->1 sum= 0 res append [-1, 0, 1] l++, r--  exit
+        
+#     skip next -1 since already handled
+    
+#     fix 0:      l->1, r->2 sum> 0 l++ exit
