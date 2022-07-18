73e145a7f8f5f76c1159e622ccc2c8f6ffa53f48
Fan Yang <1988.fan.yang@gmail.com>
2022-07-18 15:25:37 -0700

    ptyhon

--- two_pointers/941. Valid Mountain Array.java
+++ two_pointers/941. Valid Mountain Array.java
@@ -37,3 +37,15 @@ class Solution {
       return i==j && i!=n-1 && j!=0;
   }
 }
+
+
+
+PYTHON ****************************************************
+
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        left, right = 0, len(arr)-1
+        while left+1 <= len(arr)-1 and arr[left] < arr[left+1]: left+=1
+        while right-1 >= 0 and arr[right] < arr[right-1]: right-=1
+        
+        return left==right and left > 0  and right < len(arr)-1
